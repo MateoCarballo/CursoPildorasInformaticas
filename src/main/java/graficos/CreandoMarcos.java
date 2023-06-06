b00564f934e8c76014c73eca1c011454048f318f
Mateo <mateocarballo2@gmail.com>
2023-06-06 13:12:25 +0200

    video 58

--- src/main/java/graficos/CreandoMarcos.java
+++ src/main/java/graficos/CreandoMarcos.java
@@ -1,33 +1,27 @@
 package graficos;
 
 import javax.swing.*;
-// VIDEO SWING 
-public class CreandoMarcos {
-    public static void main(String[] args) {
-        miMarco marco1 = new miMarco();
-        marco1.setVisible(true);
-        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); }
-} class miMarco extends JFrame {
-    public miMarco() {
-        setSize(500,300);
-    }
-}
-
-/*
-ESTE CODIGO FUNCIONA DE LA MISMA FORMA QUE EL DE ARRIBA ES
-OTRA FORMA DE HACERLO
-
-
+// VIDEO SWING
 public class CreandoMarcos {
     public static void main(String[] args) {
         miMarco marco1 = new miMarco();
          }
 } class miMarco extends JFrame {
+
     public miMarco() {
-        setSize(500,300);
-        marco1.setVisible(true);
-        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Visibilidad
+        setVisible(true);
+        // Permitir redimensionar la ventana
+        // * setResizable(false); *
+
+        //Permitir la apertura a pantalla completa equivalente al numero 6
+        // setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        // Operacion a realizar al cerrar la aplicacion
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setBound engloba a setLocation y setSize
+        setBounds(500,300,500,500);
+        // Declaramos titulo para la ventana
+        setTitle("Mi Programa");
     }
 }
-
- */
