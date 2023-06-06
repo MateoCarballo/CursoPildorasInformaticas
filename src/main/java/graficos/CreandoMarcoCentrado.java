8cbc5afa6adaeca6b2c025aa50def6699d38b0fe
Mateo <mateocarballo2@gmail.com>
2023-06-06 13:35:03 +0200

    Video 57

---
+++ src/main/java/graficos/CreandoMarcoCentrado.java
@@ -0,0 +1,31 @@
+package graficos;
+import javax.swing.*;
+import java.awt.*;
+
+public class CreandoMarcoCentrado {
+    public static void main(String[] args) {
+        MarcoCentrado miMarcoCentrado = new MarcoCentrado();
+    }
+}
+
+    class  MarcoCentrado extends JFrame {
+
+    public MarcoCentrado(){
+        setTitle("Mi marco centrado");
+
+
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        Dimension tamanoPantalla = miPantalla.getScreenSize();
+        int alturaPantalla = tamanoPantalla.height/2;
+        int anchoPantalla = tamanoPantalla.width/2;
+
+        setSize(anchoPantalla,alturaPantalla);
+        setLocation(tamanoPantalla.width/4,tamanoPantalla.height/4);
+        Image miIcono= miPantalla.getImage("src//main//java//graficos//Prueba Icono.jpg");
+        setIconImage(miIcono);
+    }
+
+    }
