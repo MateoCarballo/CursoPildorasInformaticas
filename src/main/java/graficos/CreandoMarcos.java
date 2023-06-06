467b973923ecbe23047dfa0b05d3be6e4cf545a8
Mateo <mateocarballo2@gmail.com>
2023-06-06 12:56:05 +0200

    Agregado gitignore

---
+++ src/main/java/graficos/CreandoMarcos.java
@@ -0,0 +1,33 @@
+package graficos;
+
+import javax.swing.*;
+
+public class CreandoMarcos {
+    public static void main(String[] args) {
+        miMarco marco1 = new miMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); }
+} class miMarco extends JFrame {
+    public miMarco() {
+        setSize(500,300);
+    }
+}
+
+/*
+ESTE CODIGO FUNCIONA DE LA MISMA FORMA QUE EL DE ARRIBA ES
+OTRA FORMA DE HACERLO
+
+
+public class CreandoMarcos {
+    public static void main(String[] args) {
+        miMarco marco1 = new miMarco();
+         }
+} class miMarco extends JFrame {
+    public miMarco() {
+        setSize(500,300);
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+ */
