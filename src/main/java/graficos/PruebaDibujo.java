7e51d34b3299a5fd764ac7ccd6331fc3394c980a
Mateo <mateocarballo2@gmail.com>
2023-06-09 13:28:10 +0200

    Terminado video 60

--- src/main/java/graficos/PruebaDibujo.java
+++ src/main/java/graficos/PruebaDibujo.java
@@ -1,5 +1,6 @@
 package graficos;
 import java.awt.*;
+import java.awt.geom.*;
 import javax.swing.*;
 
 public class PruebaDibujo {
@@ -25,6 +26,32 @@ class MarcoConDibujos extends JFrame{
 class LaminaConFiguras extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        /*
+        Graphics 2D es abstracta casteamos hacia Graphics2D e intanciamos
+         como Rectangle2D
+         */
+        Graphics2D g2 = (Graphics2D) g;
+        //No instanciamos directamente la clase Rectangle2D sino la clase Rectangle2D.Double
+        //                                           POS. EN PANTALLA--DIMENSIONES DEL RECTANGULO
+        Rectangle2D rectangulo = new Rectangle2D.Double(100,100,200,150);
+        // Esta libreria es mas potente que la anterior. Tiene mas funciones.
+        g2.draw(rectangulo);
+
+        Ellipse2D elipse = new Ellipse2D.Double();
+        elipse.setFrame(rectangulo);
+        g2.draw(elipse);
+        //Podemos instanciar directamente para ahorrar lineas de codigo
+        g2.draw(new Line2D.Double(100,100,300,250 ));
+
+        double centroX= rectangulo.getCenterX();
+        double centroY= rectangulo.getCenterY();
+        double radio = 150;
+
+        Ellipse2D circulo = new Ellipse2D.Double();
+        circulo.setFrameFromCenter(centroX,centroY,
+                centroX+radio,centroY+radio);
+        g2.draw(circulo);
+        /* USADO EN EL VIDEO ANTERIOR
         //Dibuja un rectangulo
         g.drawRect(50,50,200,200);
         //Dibuja una linea
@@ -33,6 +60,7 @@ class LaminaConFiguras extends JPanel{
         g.drawArc(50,50,200,200,0,180);
         //Arco abierto arriba
         g.drawArc(50,50,200,200,180,180);
+         */
     }
 }
 
